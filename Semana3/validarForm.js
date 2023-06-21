170dd5eeba3321ea566a57f182a8950638346c07
Jhon H. Trujillo <129619193+JhonTrujillo@users.noreply.github.com>
2023-06-20 20:52:28 -0500

    Feat: Cambios actividad 3

--- Semana3/validarForm.js
+++ Semana3/validarForm.js
@@ -1,31 +1,40 @@
-const misNombres = document.getElementById("nombres");
-const misApellidos = document.getElementById("apellidos");
-const miDocumento = document.getElementById("identificacion");
-const miCorreo = document.getElementById("correo");
-const miUsuario = document.getElementById("usuario");
-const miObservaciones = document.getElementById("observaciones");
-
+class Formulario{
+    // const misNombres = document.getElementById("nombres");
+    // const misApellidos = document.getElementById("apellidos");
+    // const miDocumento = document.getElementById("identificacion");
+    // const miCorreo = document.getElementById("correo");
+    // const miUsuario = document.getElementById("usuario");
+    // const miObservaciones = document.getElementById("observaciones");
+    constructor(){
+        this.misNombres = document.getElementById("nombres")
+        this.misApellidos = document.getElementById("apellidos");
+        this.miDocumento = document.getElementById("identificacion");
+        this.miCorreo = document.getElementById("correo");
+        this.miUsuario = document.getElementById("usuario");
+        this.miObservaciones = document.getElementById("observaciones");
+    }
+}
 //validar si las cajas de texto estan vacias
 function validar() {
-    if ((misNombres.value === "")
-        || (misApellidos.value === "")
-        || (miDocumento.value === "")
-        || (miCorreo.value === "")
-        || (miUsuario.value === "")
-        || (miObservaciones.value === "")) {
+    if ((this.misNombres.value === "")
+        || (this.misApellidos.value === "")
+        || (this.miDocumento.value === "")
+        || (this.miCorreo.value === "")
+        || (this.miUsuario.value === "")
+        || (this.miObservaciones.value === "")) {
         alert("Hay campos vacios");
         // return;
     }
-    validarNombres(misNombres);
-    validarApellidos(misApellidos);
-    validarCorreo(miCorreo);
-    validarUsuario(miUsuario);
-    validarObservaciones(miObservaciones);
+    validarNombres(this.misNombres);
+    validarApellidos(this.misApellidos);
+    validarCorreo(this.miCorreo);
+    validarUsuario(this.miUsuario);
+    validarObservaciones(this.miObservaciones);
 }
 // Validar nombres maximo 60 caracteres
 function validarNombres() {
     const nombresRegex = /^([a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s ]{0,60})$/g;
-    if (nombresRegex.test(misNombres.value)) {
+    if (nombresRegex.test(this.misNombres.value)) {
         // alert("Nombres valido");
         return true;
     } else {
@@ -36,7 +45,7 @@ function validarNombres() {
 //validar apellidos maximo 60 caracteres
 function validarApellidos() {
     const apellidosRegex = /^([a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s ]{0,60})$/g;
-    if (apellidosRegex.test(misApellidos.value)) {
+    if (apellidosRegex.test(this.misApellidos.value)) {
         // alert("Apellido valido");
         return false;
     } else {
@@ -47,7 +56,7 @@ function validarApellidos() {
 //valida el correo
 function validarCorreo() {
     const correoRegex = /^([\w.]+[^#$%&\/()='"!?¡]\w*-*)([@])(\w)+(\.[a-z]{2,3})$/g;
-    if (correoRegex.test(miCorreo.value)) {
+    if (correoRegex.test(this.miCorreo.value)) {
         // alert("Correo valido");
         return true;
     } else {
@@ -58,7 +67,7 @@ function validarCorreo() {
 //validar usuario maximo 25 caracteres
 function validarUsuario() {
     const usuarioRegex = /^([a-zA-Z0-9_-]{0,25})$/g;
-    if (usuarioRegex.test(miUsuario.value)) {
+    if (usuarioRegex.test(this.miUsuario.value)) {
         // alert("Usuario valido");
         return true;
     } else{
@@ -69,7 +78,7 @@ function validarUsuario() {
 //validar observaciones maximo 200 caracteres
 function validarObservaciones() {
     const observacionesRegex = /^([a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s ]{0,20})$/g;
-    if (observacionesRegex.test(miObservaciones.value)) {
+    if (observacionesRegex.test(this.miObservaciones.value)) {
         // alert("Apellido valido");
         return false;
     } else {
