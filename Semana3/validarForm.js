05b7341f80bcc6c8eb39bde989ca50d1b85b12b9
Jhon H. Trujillo <129619193+JhonTrujillo@users.noreply.github.com>
2023-06-27 21:09:49 -0500

    Feat: Mensaje mediante etiqueta parrafo

--- Semana3/validarForm.js
+++ Semana3/validarForm.js
@@ -4,51 +4,55 @@ const miDocumento = document.getElementById("identificacion");
 const miCorreo = document.getElementById("correo");
 const miUsuario = document.getElementById("usuario");
 const miObservaciones = document.getElementById("observaciones");
-var error = document.getElementById("formulario_input_error");
-
-// form.addEventListener("submit", (e) =>{
-//      e.preventDefault();
-// });
-
+const errorNombre = document.getElementById("error_nombres")
+const errorApellido = document.getElementById("error_apellidos")
+const errorCorreo = document.getElementById("error_correo")
+const errorUsuario = document.getElementById("error_usuario")
+const errorObservacion = document.getElementById("error_observaciones")
+const errorVacios = document.getElementById("error_vacios")
+const mensajeExito = document.getElementById("mensaje_exito")
 //validar si las cajas de texto estan vacias
-function validar() {
-    if((misNombres.value === "") 
-            || (misApellidos.value === "")
-            || (miDocumento.value === "")
-            || (miCorreo.value === "")
-            || (miUsuario.value === "")
-            || (miObservaciones.value === "")){
-        alert("Hay campos vacios");
-        // let elemento = document.getElementById("nombres");
-        // elemento.lastElementChild.innerHTML = "Nombre no valido";
-        // return;
+function validar(event) {
+    event.preventDefault();
+    if ((misNombres.value === "")
+        || (misApellidos.value === "")
+        || (miDocumento.value === "")
+        || (miCorreo.value === "")
+        || (miUsuario.value === "")
+        || (miObservaciones.value === "")) {
+        errorVacios.classList.add("block");
+    } else {
+        mensajeExito.classList.add("block")
+        errorVacios.classList.remove("block");
     }
 
-validarNombres(misNombres);
-validarApellidos(misApellidos);
-validarCorreo(miCorreo);
-validarUsuario(miUsuario);
-validarObservaciones(miObservaciones);
+    validarNombres(misNombres);
+    validarApellidos(misApellidos);
+    validarCorreo(miCorreo);
+    validarUsuario(miUsuario);
+    validarObservaciones(miObservaciones);
 }
 // Validar nombres maximo 60 caracteres
 function validarNombres() {
     const nombresRegex = /^([a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s ]{0,60})$/g;
     if (nombresRegex.test(misNombres.value)) {
-        // alert("Nombres valido");
+        errorNombre.classList.remove("block");
         return true;
     } else {
-        alert("sobrepasa los cantidad de caracteres permitidos para nombre");
-        return false;
+        errorNombre.classList.add("block");
+        // alert("sobrepasa los cantidad de caracteres permitidos para nombre");
+        // return false;
     }
 }
 //validar apellidos maximo 60 caracteres
 function validarApellidos() {
     const apellidosRegex = /^([a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s ]{0,60})$/g;
     if (apellidosRegex.test(misApellidos.value)) {
-        // alert("Apellido valido");
+        errorApellido.classList.remove("block");
         return false;
     } else {
-        alert("sobrepasa los cantidad de caracteres permitidos para apellido");
+        errorApellido.classList.add("block");
+        // alert("sobrepasa los cantidad de caracteres permitidos para apellido");
         return true;
     }
 }
@@ -57,9 +61,11 @@ function validarCorreo() {
     const correoRegex = /^([\w.]+[^#$%&\/()='"!?¡]\w*-*)([@])(\w)+(\.[a-z]{2,3})$/g;
     if (correoRegex.test(miCorreo.value)) {
         // alert("Correo valido");
+        errorCorreo.classList.remove("block");
         return true;
     } else {
-        alert("Correo Invalido");
+        // alert("Correo Invalido");
+        errorCorreo.classList.add("block");
         return false;
     }
 }
@@ -68,9 +74,11 @@ function validarUsuario() {
     const usuarioRegex = /^([a-zA-Z0-9_-]{0,25})$/g;
     if (usuarioRegex.test(miUsuario.value)) {
         // alert("Usuario valido");
+        errorUsuario.classList.remove("block");
         return true;
     } else {
-        alert("sobrepasa los cantidad de caracteres permitidos para usuario");
+        // alert("sobrepasa los cantidad de caracteres permitidos para usuario");
+        errorUsuario.classList.add("block")
         return false;
     }
 }
@@ -79,9 +87,11 @@ function validarObservaciones() {
     const observacionesRegex = /^([a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s ]{0,20})$/g;
     if (observacionesRegex.test(miObservaciones.value)) {
         // alert("Apellido valido");
+        errorObservacion.classList.remove("block");
         return false;
     } else {
-        alert("sobrepasa los cantidad de caracteres permitidos para observaciones");
+        errorObservacion.classList.add("block");
+        // alert("sobrepasa los cantidad de caracteres permitidos para observaciones");
         return true;
     }
 }
